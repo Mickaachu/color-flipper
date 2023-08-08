a64763039ee31a5037283f88871987a89f2a5649
Erin Capuchino <valleerin4@gmail.com>
2023-08-08 12:16:32 +0800

    change color

--- App.js
+++ App.js
@@ -1,20 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {Text, View, Button} from 'react-native';
+import { useEffect, useState } from 'react';
 
 export default function App() {
+  const [color,setColor] = useState('');
+  const changeColor = () => {
+    const randomColor = Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, '0');
+    setColor(`#${randomColor}`);
+    
+  }
+  useEffect
+  console.log(color)
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+      backgroundColor: color,
+    }}>
+      <Text>
+        Click Button to change color
+      </Text>
+      <Button title="Change color" onPress={changeColor}/>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
